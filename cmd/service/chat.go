20d1a2058a1de0caf3293e5fa00f38aba5572984
Cu pid <kelanidarasimi9@gmail.com>
2026-01-09 12:14:34 +0100

    bug: fix missing TypingEvent error in proto file

--- cmd/service/chat.go
+++ cmd/service/chat.go
@@ -1,4 +1,9 @@
 package service
 
+import red "github.com/lupppig/grpc-cli-chat/internal/redis"
+
 type ChatServer struct {
+	RedisCl *red.Redis
 }
+
+
--- makefile
+++ makefile
@@ -1,6 +1,5 @@
 .SILENT:
 
-
 gen-go:
 	protoc --proto_path=proto \
     --go_out=pb --go_opt=paths=source_relative \
